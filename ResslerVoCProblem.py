704138518a12d67c07dd6c539e1dab55527645f8
= <=>
2025-11-10 23:00:28 -0500

    Testing commit

---
+++ ResslerVoCProblem.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Problem. In season 8 of Blacklist, Ressler describes the following as getting 20% closer to finding liz
+
+# They had five suspects for who could be "Rakitin", but before the FBI made a guess as to who Rakitin was, Rakitin had one
+# of the five suspects killed. The FBI then made a guess as to who Rakitin was, but Rakitin had another one of the suspects killed.
+# So, at the beginning, they had a 20% chance of finding Rakitin. But after the first suspect was killed, they had a 25% chance of finding Rakitin.
+# Ressler then says they are 20% closer to finding Rakitin. I think he is wrong.
+
+# He was using variable change to calculate the percentage change, but he should have been using absolute change.
+
+print("test")
+
+print("Push Test")
